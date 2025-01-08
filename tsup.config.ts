bda7cd87ce3c9fea72d3ddb66aa580cd107fbfb8
Cweili <3weili@gmail.com>
2025-01-08 16:43:50 +0800

    chore: build config

--- tsup.config.ts
+++ tsup.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
   ],
   dts: true,
   clean: true,
+  cjsInterop: true,
   define: {
     'process.env.NODE_ENV': JSON.stringify('production'),
     'process.env.JEST_WORKER_ID': JSON.stringify(false),
